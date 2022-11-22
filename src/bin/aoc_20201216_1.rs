08cfc019f1c504ed724ca2e591810d655afe9b35
Benno Richters <b.richters@gmail.com>
2022-11-22 21:27:27 +0100

    working on day 16

--- src/bin/aoc_20201216_1.rs
+++ src/bin/aoc_20201216_1.rs
@@ -23,15 +23,17 @@ fn solve() {
     let lines = lines_from_file("tin");
 
     let mut iter = lines.split(|e| e.is_empty());
-    // println!("{:?}", iter.next().unwrap());
-    // println!("{:?}", iter.next());
-    // println!("{:?}", iter.next());
     let a = field_ranges(iter.next().unwrap());
     println!("{:?}", a);
+
+    iter.next(); // ignore this ticket section
+    
+    let b = ticket_numbers(iter.next().unwrap());
+    println!("{:?}", b);
 }
 
-fn field_ranges(lines: &[String]) -> Vec<((u32, u32), (u32, u32))> {
-    let mut result: Vec<((u32, u32), (u32, u32))> = Vec::new();
+fn field_ranges(lines: &[String]) -> Vec<(u32, u32)> {
+    let mut result: Vec<(u32, u32)> = Vec::new();
     for line in lines {
         let key_value: Vec<&str> = line.split(": ").collect();
         let value = key_value[1];
@@ -40,18 +42,27 @@ fn field_ranges(lines: &[String]) -> Vec<((u32, u32), (u32, u32))> {
         let first: Vec<&str> = ranges[0].split("-").collect();
         let second: Vec<&str> = ranges[1].split("-").collect();
 
-        let entry = (
-            (
-                first[0].parse::<u32>().unwrap(),
-                first[1].parse::<u32>().unwrap(),
-            ),
-            (
-                second[0].parse::<u32>().unwrap(),
-                second[1].parse::<u32>().unwrap(),
-            ),
-        );
+        result.push((
+            first[0].parse::<u32>().unwrap(),
+            first[1].parse::<u32>().unwrap(),
+        ));
+        result.push((
+            second[0].parse::<u32>().unwrap(),
+            second[1].parse::<u32>().unwrap(),
+        ));
+    }
 
-        result.push(entry);
+    result
+}
+
+fn ticket_numbers(lines: &[String]) -> Vec<u32> {
+    let mut result: Vec<u32> = Vec::new();
+
+    for line in &lines[1..] {
+        let nrs: Vec<&str> = line.split(",").collect();
+        for nr in nrs {
+            result.push(nr.parse::<u32>().unwrap());
+        }
     }
 
     result
