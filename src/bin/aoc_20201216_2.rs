c220f77efdc009d32d3ae4191b1dd87e73f98aa3
Benno Richters <bennor@topdesk.com>
2022-11-24 17:23:20 +0100

    working on day 16 part 2

--- src/bin/aoc_20201216_2.rs
+++ src/bin/aoc_20201216_2.rs
@@ -2,6 +2,7 @@
 #![allow(unused_variables)]
 
 use std::{
+    collections::HashMap,
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
@@ -28,12 +29,17 @@ fn solve() {
     // println!("{:?}", iter.next());
     let a = field_ranges(iter.next().unwrap());
     println!("{:?}", a);
+
+    let b = ticket_numbers(iter.next().unwrap());
+    println!("{:?}", b);
 }
 
-fn field_ranges(lines: &[String]) -> Vec<((u32, u32), (u32, u32))> {
-    let mut result: Vec<((u32, u32), (u32, u32))> = Vec::new();
+type Field = ((u32, u32), (u32, u32));
+fn field_ranges(lines: &[String]) -> HashMap<&str, Field> {
+    let mut result: HashMap<&str, Field> = HashMap::new();
     for line in lines {
         let key_value: Vec<&str> = line.split(": ").collect();
+        let key = key_value[0];
         let value = key_value[1];
         let ranges: Vec<&str> = value.split(" or ").collect();
 
@@ -51,7 +57,22 @@ fn field_ranges(lines: &[String]) -> Vec<((u32, u32), (u32, u32))> {
             ),
         );
 
-        result.push(entry);
+        result.insert(key, entry);
+    }
+
+    result
+}
+
+fn ticket_numbers(lines: &[String]) -> HashMap<u32, Vec<u32>> {
+    let mut result: HashMap<u32, Vec<u32>> = HashMap::new();
+    for (r, line) in lines[1..].iter().enumerate() {
+        let nrs: Vec<&str> = line.split(",").collect();
+        for (c, nr) in nrs.iter().enumerate() {
+            result
+                .entry(c as u32)
+                .or_insert(Vec::new())
+                .push(nr.parse::<u32>().unwrap());
+        }
     }
 
     result
