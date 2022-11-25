13c643e671f6bc5c5791baa20cd3dd7c54c3a375
Benno Richters <b.richters@gmail.com>
2022-11-25 13:21:48 +0100

    working on day 16 part 2

--- src/bin/aoc_20201216_2.rs
+++ src/bin/aoc_20201216_2.rs
@@ -18,32 +18,83 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 
 fn main() {
     solve();
+    // let a = vec![0, 1, 2, 3, 4, 5];
+    // let x = 5;
+    // let b = [&a[..x], &a[(x + 1)..]].concat();
+    // println!("{:?}", b);
 }
 
+type Field = ((u32, u32), (u32, u32));
+
 fn solve() {
-    let lines = lines_from_file("tin");
+    let lines = lines_from_file("in");
 
     let mut iter = lines.split(|e| e.is_empty());
     let fields = field_ranges(iter.next().unwrap());
-    println!("{:?}", fields);
+    // println!("{:?}", fields);
 
     let field_values: Vec<[(u32, u32); 2]> = fields.values().map(|e| [e.0, e.1]).collect();
     let ranges: Vec<(u32, u32)> = field_values.concat();
-    println!("{:?}", ranges);
 
     let your = iter.next().unwrap();
     let nearby = iter.next().unwrap();
     let parsed = parse_numbers(&([&your[1..], &nearby[1..]].concat()));
-    println!("{:?}", parsed);
 
     let valid = valid_tickets(parsed, ranges);
-    println!("{:?}", valid);
 
     let columns = numbers_per_column(valid);
-    println!("{:?}", columns);
+    // println!("{:?}", columns);
+
+    process(fields, columns);
 }
 
-type Field = ((u32, u32), (u32, u32));
+struct Data<'a> {
+    fields: &'a HashMap<&'a str, Field>,
+    columns: &'a HashMap<u32, HashSet<u32>>,
+}
+
+impl Data<'_> {
+    fn rec_process(&self, field_options: &[&str], solution: Vec<&str>) {
+        // println!("{:?} ---- {:?}", field_options, solution);
+        if field_options.is_empty() {
+            println!("----> {:?}", solution);
+            return;
+        }
+
+        let column = solution.len() as u32;
+        for (i, field) in field_options.iter().enumerate() {
+            if self.is_possible(field, self.columns.get(&column).unwrap()) {
+                let rec_fields = &[&field_options[..i], &field_options[(i + 1)..]].concat();
+                let mut rec_solution = solution.clone();
+                rec_solution.push(&field);
+                self.rec_process(rec_fields, rec_solution);
+            }
+        }
+    }
+
+    fn is_possible(&self, field: &str, column_numbers: &HashSet<u32>) -> bool {
+        let ranges: &Field = self.fields.get(field).unwrap();
+        let r1 = ranges.0 .0..(ranges.0 .1 + 1);
+        let r2 = ranges.1 .0..(ranges.1 .1 + 1);
+
+        column_numbers
+            .iter()
+            .all(|&e| r1.contains(&e) || r2.contains(&e))
+    }
+}
+
+fn process(fields: HashMap<&str, Field>, columns: HashMap<u32, HashSet<u32>>) {
+    let data = Data {
+        fields: &fields,
+        columns: &columns,
+    };
+    let field_options = fields.keys().cloned().collect::<Vec<&str>>();
+    let solution: Vec<&str> = Vec::new();
+
+    println!("{:?}", field_options);
+    data.rec_process(&field_options, solution);
+}
+
 fn field_ranges(lines: &[String]) -> HashMap<&str, Field> {
     let mut result: HashMap<&str, Field> = HashMap::new();
     for line in lines {
